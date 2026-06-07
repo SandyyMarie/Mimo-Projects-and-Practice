98af272da83ab84990d05a8f25f1b6a107464699
Sandy Lopez <59062958+SandyyMarie@users.noreply.github.com>
2026-06-07 00:35:29 -0600

    Add unit conversion function for miles to kilometers

---
+++ JavaScript/UnitConverterProject/script.js
@@ -0,0 +1,15 @@
+function convert() {
+  let inputValue = document.getElementById("userInput").value;
+  let unit = document.getElementById("unit").value;
+  milesToKm = (unit === "milesToKm");
+  result = 0;
+  if (milesToKm) {
+    result = inputValue * 1.60934;
+  } else {
+    result = inputValue / 1.60934;
+  }
+  const resultString = inputValue + " miles are " + result + " km";
+  console.log(resultString);
+  const resultElement = document.getElementById('resultElement');
+  resultElement.innerHTML = resultString;
+}
